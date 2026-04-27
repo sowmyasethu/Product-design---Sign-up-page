5f892f597dac6a220e8e547ad91b69a71da7630b
sowmyasethu <sowmyasethu007@gmail.com>
2026-04-27 19:02:51 +0530

    return back to old

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <div className="bg-black w-full max-w-md p-8 rounded-xl shadow-xl">
+      <div className="bg-white w-full max-w-md p-8 rounded-xl shadow-xl">
 
         {/* Logo / Title */}
         <h1 className="text-center text-3xl font-bold text-blue-600">
@@ -12,7 +12,7 @@ export default function Home() {
 
         {/* Subtitle */}
         <h2 className="text-center text-xl font-semibold mt-4">
-          Sign-In to IAM Secure
+          Sign In to IAM Secure
         </h2>
 
         <p className="text-center text-gray-500 mt-2 mb-6">
